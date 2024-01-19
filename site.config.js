d0008fbcb4b9b873851c4e887fe9b43dc51e1576
JeonghyeonYu <50809987+minion057@users.noreply.github.com>
2024-01-19 22:59:00 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,14 +1,14 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    name: "Jeonghyeon",
+    image: "/profile-image.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "AI developer",
+    bio: "공부하자!",
+    email: "getit3981@gmail.com",
+    //linkedin: "none",
+    github: "minion057",
+    //instagram: "none",
   },
   projects: [
     {
@@ -18,14 +18,14 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "jeonghyeon-log",
+    description: "welcome to jeonghyeon-log!",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://jhyu-tech-blog.vercel.app",
+  since: 2024, // If leave this empty, current year will be used.
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
